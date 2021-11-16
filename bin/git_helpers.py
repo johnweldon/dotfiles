6636eacb0a0665d72133ebe0a8323d9d5913c182
John Weldon <jweldon@vmware.com>
2021-11-16 13:15:39 -0700

    more defaults

--- bin/git_helpers.py
+++ bin/git_helpers.py
@@ -2,12 +2,14 @@ import os
 import subprocess
 
 
-ignored_dirs = list(filter(None, ["node_modules", ".terraform", "dist"] + os.getenv("GIT_ALL_IGNORE").split(":")))
+ignored_dirs = list(filter(None, ["node_modules", ".terraform", "dist", ".git", ".idea"] + os.getenv("GIT_ALL_IGNORE").split(":")))
 
 
 def all_git_dirs(action: str, start_dir="."):
     for root, dirs, files in os.walk(start_dir):
         for ig in ignored_dirs:
+            if root.endswith(f"/{ig}"):
+                break
             if f"/{ig}/" in root:
                 break
         else:
