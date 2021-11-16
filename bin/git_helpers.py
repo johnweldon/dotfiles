c49631e4d724228f67da121a581373189daf1c4d
John Weldon <johnweldon4@gmail.com>
2021-11-16 13:02:41 -0700

    Use GIT_ALL_IGNORE to skip dirs

--- bin/git_helpers.py
+++ bin/git_helpers.py
@@ -2,10 +2,15 @@ import os
 import subprocess
 
 
+ignored_dirs = list(filter(None, ["node_modules", ".terraform", "dist"] + os.getenv("GIT_ALL_IGNORE").split(":")))
+
+
 def all_git_dirs(action: str, start_dir="."):
     for root, dirs, files in os.walk(start_dir):
-        if "node_modules" in root:
-            continue
-        if ".git" in dirs:
-            cmd = f"bash -lc '( cd {root}; {action} )'"
-            subprocess.run(cmd, shell=True)
+        for ig in ignored_dirs:
+            if f"/{ig}/" in root:
+                break
+        else:
+            if ".git" in dirs:
+                cmd = f"bash -lc '( cd {root}; {action} )'"
+                subprocess.run(cmd, shell=True)
