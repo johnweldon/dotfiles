9aa47f99db210361b94dc956a88ccdc0df614f74
John Weldon <johnweldon4@gmail.com>
2025-05-14 11:49:14 -0700

    Simplify blink

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -2,33 +2,33 @@ require("lib")
 
 return {
   "saghen/blink.cmp",
-  dependencies = { "ribru17/blink-cmp-spell" },
-  opts = function(_, opts)
-    --print(DebugDump(opts.sources))
-    opts.sources.providers["spell"] = {
-      name = "Spell",
-      module = "blink-cmp-spell",
-      opts = {},
-    }
-
-    -- default sources:
-    --  1: lsp,
-    --  2: path,
-    --  3: snippets,
-    --  4: buffer,
-    --  5: lazydev,
-    --  6: dadbod,
-    opts.sources.per_filetype = { markdown = { "lsp", "path", "snippets", "lazydev", "dadbod", "spell" } }
-    opts.sources.min_keyword_length = 4
-
-    local trigger = opts.completion.trigger or {}
-    trigger.show_on_keyword = true
-    opts.completion.trigger = trigger
-
-    opts.completion.menu.auto_show = false
-
-    local list = opts.completion.list or { selection = {} }
-    list.selection = { preselect = true, auto_insert = true }
-    opts.completion.list = list
-  end,
+  opts = {
+    completion = {
+      trigger = {
+        show_on_keyword = true,
+        show_on_insert_on_trigger_character = true,
+      },
+      menu = {
+        auto_show = true,
+      },
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = true,
+        },
+      },
+    },
+    fuzzy = {
+      sorts = {
+        "exact",
+        "score",
+        "sort_text",
+      },
+    },
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+  },
 }
