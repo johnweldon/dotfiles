45d4f80e2f89454675227f83f6c38e13d36728c9
John Weldon <johnweldon4@gmail.com>
2025-08-11 14:31:51 -0700

    Support Terminal.app

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,3 +2,9 @@ require("config.lazy")
 
 vim.opt.autowriteall = true
 vim.opt.exrc = true
+
+local termProg = os.getenv("TERM_PROGRAM")
+if termProg == "Apple_Terminal" then
+  vim.opt.termguicolors = false
+  vim.cmd("colorscheme koehler")
+end
