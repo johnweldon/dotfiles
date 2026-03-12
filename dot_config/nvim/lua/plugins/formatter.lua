9305ff2d566c46cb35968daf230fac8e51767808
John Weldon <johnweldon4@gmail.com>
2026-03-11 22:29:06 -0700

    Add formatters for c, cpp, json, lua, python, rust, sh, terraform, toml, yaml, and zig

--- dot_config/nvim/lua/plugins/formatter.lua
+++ dot_config/nvim/lua/plugins/formatter.lua
@@ -6,8 +6,19 @@ return {
         timeout_ms = 6000,
       },
       formatters_by_ft = {
+        c = { "clang_format" },
+        cpp = { "clang_format" },
         go = { "goimports", "gofumpt" },
+        json = { "jq" },
+        lua = { "stylua" },
         markdown = { "prettier_markdown", "unescape_globs" },
+        python = { "black", "ruff_fix" },
+        rust = { "rustfmt" },
+        sh = { "shfmt" },
+        terraform = { "terraform_fmt" },
+        toml = { "taplo" },
+        yaml = { "yq" },
+        zig = { "zigfmt" },
       },
       formatters = {
         prettier_markdown = {
@@ -18,6 +29,12 @@ return {
             "--parser",
             "markdown",
           },
+        },
+        jq = {
+          args = { "--indent", "2", "." },
+        },
+        shfmt = {
+          prepend_args = { "-i", "2" },
         },
         unescape_globs = {
           command = "sed",
